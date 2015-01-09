861cc304b9ec8d3f09bafce1a9474aed1258d750
Andrey Byelogurov <byelogurov@gmail.com>
2015-01-09 14:44:28 -0800

    Finished basic hw1

--- Homeowork1/main.hs
+++ Homeowork1/main.hs
@@ -4,25 +4,14 @@ module Homework2 where
 
 import           Excell
 
-tab1 = row['A','P','r','i','l',' ',' ']
-tab2 = row["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
-tab3 = row["","","","1","2","3","4"]
-tab4 = row["5","6","7","8","9","10","11"]
-tab5 = row["12","13","14","15","16","17","18"]
-tab6 = row["19","20","21","22","23","24","25"]
-tab7 = row["26","27","28","29","30","",""]
+month  = row['A','p','r','i','l',' ',' '::Char]
+days = row["Sun","Mon","Tue","Wed","Thu","Fri","Sat"::String]
 
-tab8 = tab1 `above` tab2 `above` tab3 `above` tab4 `above` tab5 `above` tab6 `above` tab7
-main = print tab8
+week1 = blankRow 3 `beside` row[1,2,3,4::Integer]
+week2 = row[5,6,7,8,9,10,11::Integer]
+week3 = row[12,13,14,15,16,17,18::Integer]
+week4 = row[19,20,21,22,23,24,25::Integer]
+week5 = row[26,27,28,29,30::Integer]
 
-
-{-
-
-stringToInt x1 = (sum . map mult . powers . map decimal) x1
-
-mult (x,y) = x * y
-powers xs = zip [ 10^i | i <- [0..]] (reverse xs)
-decimal  x
-	| x <= '9' && x >= '0' = ord x - ord '0'
-	| True = error ("Bad char in decimal "++[x])
--}
+final = month `above` days `above` week1 `above` week2 `above` week3 `above` week4 `above` week5
+calendar = print final
