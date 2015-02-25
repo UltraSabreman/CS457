ecb39b07bb2c88e127dda8a0e68da56fe35401e7
Andrey Byelogurov <byelogurov@gmail.com>
2015-02-24 22:26:47 -0800

    Finished hw6
    yay

--- Homework6/main.hs
+++ Homework6/main.hs
@@ -3,140 +3,9 @@ CS457/557 Functional Languages                Type Classes (Homework 6)
 -----------------------------------------------------------------------
 Name of programmer: Andrey Byelogurov
 Email to send comments to: byelogurov@gmail.com
-
-Consider the following class definition, which is designed to
-characterize the set of types that contain only finitely many
-distinct elements:
-
-> class Finite a where
->   elements :: [a]
-
-For example, we can certainly enumerate the set of all Bool
-values, so we might declare the following instance:
-
-> instance Finite Bool where
->   elements = [ False, True ]
-
-We can repeat this same idea for other enumerated types:
-
-> data Day     = Sun | Mon | Tue | Wed | Thu | Fri | Sat
-> data Month   = Jan | Feb | Mar | Apr | May | Jun
->              | Jul | Aug | Sep | Oct | Nov | Dec
-> data Rainbow = Red | Orange | Yellow | Green | Blue | Indigo | Violet
-
-Of course, all of these types are finite:
-
-> instance Finite Day where
->   elements = [Sun, Mon, Tue, Wed, Thu, Fri, Sat]
-> instance Finite Month where
->   elements = [ Jan, Feb, Mar, Apr, May, Jun,
->                Jul, Aug, Sep, Oct, Nov, Dec]
-> instance Finite Rainbow where
->   elements = [Red, Orange, Yellow, Green, Blue, Indigo, Violet]
-
-As a slightly more interesting example, consider the Maybe datatype
-that is defined in the prelude as follows:
-
-  data Maybe a = Nothing | Just a
-
-If we can enumerate the elements of some type t, then we can also
-enumerate all the elements of Maybe t, as the following instance
-describes:
-
-> instance Finite a => Finite (Maybe a) where
->   elements = [ Nothing ] ++ [ Just x | x <- elements ]
-
-A critical property that we would like to hold for each instance t of
-Finite is that any element x of type t will appear in the corresponding
-list elements :: [t].  For types t that are also in the Eq class, we
-might try to capture this with a QuickCheck property like the following:
-
-> prop_AllElementsAppear n  =  n `elem` elements
-
-Note that this law is stated in a very general form; if you try to run
-quickCheck prop_AllElementsAppear directly, then you will get an error
-message because QuickCheck won't know which element type to use.  You
-will therefore need to find a way to insert type information into your
-tests, as in: quickCheck (prop_AllElementsAppear :: Bool -> Bool).
-
-(Also note: For the purposes of this exercise: if you know about the
-possibility of types that include a bottom element---representing an
-error or nontermination, for example---then you should ignore those
-cases here ... which, of course, is also what you would have done if you
-haven't heard about this issue ...)
-
-Now here are the questions:
-
-1) Define instances of Finite for the Int type (whose minimum and
-maximum values can be obtained as (minBound::Int) and (maxBound::Int),
-respectively), the product type (a, b) (assuming, of course that both
-a and b are finite), and the sum type Either a b, which is defined in
-the prelude as:
-
-   data Either a b = Left a | Right b
-
-Explain how your definitions work and use QuickCheck to verify that
-your definitions satisfy the proposed law, at least on the basis of
-some random tests.  [Note that a finite type can still have many
-elements; you might want to think carefully about how you write your
-instance definitions, especially the one for Int, to ensure that
-testing does not take too long!]
-
-2) If a is a Finite type, then we can use elements :: [a] to obtain
-the list of all values in the domain of a function of type a -> b.
-Use this observation to define the following instances for displaying
-functions (you may use whatever notation you prefer) and for testing
-functions for equality:
-
-  instance (Finite a, Show a, Show b) => Show (a -> b) where
-    show f = ...
-
-  instance (Finite a, Eq b) => Eq (a -> b) where
-    f == g  =  ...
-
-You may, of course, define any auxiliary functions that you need to
-make these definitions work as you would expect.
-
-3) Devise an algorithm for calculating a list of all functions of type
-a -> b for any finite types a and b.  Show that this can be wrapped
-up as an instance of the Finite type class:
-
-  instance (Eq a, Finite a, Finite b) => Finite (a -> b) where
-    ...
-
-Again, use QuickCheck to verify that your definition satisfies the law
-specified in the introduction.
-
-4) Suppose that we want to be able to calculate the number of elements
-in any given finite type.  This can be accomplished by adding the
-following lines to the end of the definition of the Finite class:
-
-   size  :: a -> Integer
-   size x = length (x:elements) - 1
-
-Explain why the definition was written in this (rather odd) manner
-instead of just writing:
-
-   size  :: Integer
-   size   = length elements
-
-(If you're not sure why this wouldn't work, maybe you should enter it
-into your code and see what happens ...)
-
-Show that you can obtain considerably more efficient definitions for
-size by overriding the implementation of size in specific instances of
-the Finite class.
-
-5) Use QuickCheck to formulate and test some properties about size for
-Maybe types, product types (pairs), and sum types (Either).  For
-example, if the finite type t has n elements, then how many elements
-would you expect to find in the type Maybe t?  How useful is QuickCheck
-in cases like this?
-
 -------------------------------------------------------------------------}
-module Homework5 where
+module Homework6 where
 
-import Test.HUnit
 import Test.QuickCheck
 
 {-
@@ -157,28 +26,32 @@ testing does not take too long!]
 
 class Finite a where
   elems :: [a]
-  --size :: a -> Int
+  size :: a -> Int
+  size x = length (x:elems) - 1
+
+instance Finite a => Finite (Maybe a) where
+	elems = [ Nothing ] ++ [ Just x | x <- elems ]
 
 instance Finite Bool where
   elems = [True, False]
-  --size x = 2
+  size x = 2
 --Simply returns a list with Ture and false.
 
 instance Finite Int where
-  elems = [y | x <- [0..maxBound::Int], y <- [x,-x]]
-  --size x = x
+  elems = 0 : [y | x <- [1..maxBound::Int], y <- [x,-x]]
+  size x = x
 --returns all ints. It's actulay kinda cool how this works,
 --as it builds the list out from 0.
 
 instance (Finite a, Finite b) => Finite (a, b) where
   elems = [ (x,y) | x <- elems, y <- elems]
-  --size (x, y) = (size x) * (size y)
+  size (x, y) = (size x) * (size y)
 --All possible sets of the two elems sets crossed together
 
 instance (Finite a, Finite b) => Finite (Either a b) where
   elems = [Left x | x <- elems] ++ [Right x | x <-elems]
-  --size (Left x) = size x
-  --size (Right x) = size x
+  size (Left x) = size x
+  size (Right x) = size x
 --A list of both lists combined.
 
 
@@ -191,12 +64,13 @@ prop_TestPow x = elem x elems
 --prop_TestAdd :: (Either a b) -> Bool
 --prop_TestAdd (Left x) = elem x elems
 --Not sure how to build a test for this.....
+
 {-
 2)If a is a Finite type, then we can use elements :: [a] to obtain
 the list of all values in the domain of a function of type a -> b.
 Use this observation to define the following instances for displaying
 functions (you may use whatever notation you prefer) and for testing
-functions for equality:
+functions for equality:Ok
 
   instance (Finite a, Show a, Show b) => Show (a -> b) where
     show f = ...
@@ -208,12 +82,89 @@ You may, of course, define any auxiliary functions that you need to
 make these definitions work as you would expect.
 -}
 
-someTest :: Int -> Bool
-someTest 0 = False
-someTest _ = True
+instance (Finite a, Show a, Show b) => Show (a -> b) where
+	show f =  concat [ show x ++ " => " ++ show (f x) ++ "|" | x <- elems]
 
-testSome :: (Int -> Bool) -> Bool
-testSome (i, b) = True
+instance (Finite a, Eq b) => Eq (a -> b) where
+	f == g  = and [f x  ==  g x | x <- elems]
 
---instance (Finite a, Show a, Show b) => Show (a -> b) where
---	show (x -> y) = undefined --show $ id f --show f ++ " -> "  ++ show b
+
+{-
+3) Devise an algorithm for calculating a list of all functions of type
+a -> b for any finite types a and b.  Show that this can be wrapped
+up as an instance of the Finite type class:
+
+  instance (Eq a, Finite a, Finite b) => Finite (a -> b) where
+    ...
+
+Again, use QuickCheck to verify that your definition satisfies the law
+specified in the introduction.
+-}
+
+instance (Eq a, Finite a, Finite b) => Finite (a -> b) where
+	elems = [ (\x -> y) | y <- elems] 
+
+prop_TestFun :: (Finite a, Finite b, Eq a, Eq b) => (a -> b)-> Bool
+prop_TestFun x = elem x elems
+
+-- Frankly I have no idea what you mean by list of functions. This is my best
+-- guess but I can't come up with a good way to test it.
+
+{-
+4) Suppose that we want to be able to calculate the number of elements
+in any given finite type.  This can be accomplished by adding the
+following lines to the end of the definition of the Finite class:
+
+   size  :: a -> Integer
+   size x = length (x:elements) - 1
+
+Explain why the definition was written in this (rather odd) manner
+instead of just writing:
+
+   size  :: Integer
+   size   = length elements
+
+(If you're not sure why this wouldn't work, maybe you should enter it
+into your code and see what happens ...)
+
+Show that you can obtain considerably more efficient definitions for
+size by overriding the implementation of size in specific instances of
+the Finite class.
+-}
+
+-- Becuase we need to figure out what instance of elements we want first
+-- and we do that by concating it with x. We also need to take one off the size
+-- because of that as well.
+
+-- For bools we can just return 2 cause there are only 2
+-- for product it's much faster to simply multiply the sizes of the two sides
+-- and for adition the same thing happens.
+
+
+{-
+5) Use QuickCheck to formulate and test some properties about size for
+Maybe types, product types (pairs), and sum types (Either).  For
+example, if the finite type t has n elements, then how many elements
+would you expect to find in the type Maybe t?  How useful is QuickCheck
+in cases like this?
+-}
+
+-- for Maybe
+-- I'd expect to find N + 1, the set would contain nothing, and then just 0, just 1...just n
+
+prop_sizeBool :: Bool -> Bool
+prop_sizeBool b = (size (Just b)) == ((size b) + 1)
+
+prop_sizeInt :: Int -> Bool
+prop_sizeInt b = (size (Just b)) == ((size b) + 1)
+
+
+prop_sizeProd :: (Bool, Bool) -> Bool
+prop_sizeProd (x,y) = (size (x,y)) == ((size x) * (size y))
+
+--For add, the size is just either the size of the left or right, not much to test.
+
+
+--QuickCheck is ok here, because it gives us a really breif glance at weather or not
+--our laws are good. Ofcourse further detailed testing would be required for things
+--that aren't trivial, but it's "good enough"
