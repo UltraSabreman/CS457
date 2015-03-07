a17190a3d884fc65cf8a707cfb4ade8bc4680bb4
Andrey Byelogurov <byelogurov@gmail.com>
2015-03-06 23:55:11 -0800

    Removed test file

--- project/test.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-import Data.Char
-import Foreign.C.Types
-getHiddenChar = fmap (chr.fromEnum) c_getch
-foreign import ccall unsafe "conio.h getch"
-  c_getch :: IO CInt
