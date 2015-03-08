ff5e065de5a3dcf475534d9cfac5f2fb3ab5c778
Andrey Byelogurov <byelogurov@gmail.com>
2015-03-08 00:37:56 -0800

    Fixed a bunch of shit + formatting
    Fixed a bunch of WTF bugs (no clue how it worked before).
    
    Converted to Cell data type
    Implimenetd most suggestions provided
    Cleaned up alot of code
    Commented code.

--- project/main.hs
+++ project/main.hs
@@ -9,137 +9,175 @@ import System.Random
 import System.IO
 import System.Console.Haskeline
 import Control.Monad.IO.Class
+import Test.HUnit
 
-trueFieldSize :: [[Int]] -> (Int, Int)
+{---------------------}
+{- Utility Fucntions -}
+{---------------------}
+
+--Custom cell data type
+data Cell = Alive | Dead deriving (Show, Eq)
+
+--This gets the actual size of the field
+--Used for calculations
+trueFieldSize :: [[Cell]] -> (Int, Int)
 trueFieldSize field = (length field, length $ field !! 0)
 
-fieldSize :: [[Int]] -> (Int, Int)
+--This gets the size of the field -1 on both dimentions
+--Used for iteration and limits
+fieldSize :: [[Cell]] -> (Int, Int)
 fieldSize field = (fst size - 1, snd size - 1)
     where size = trueFieldSize field
 
-testField :: [[Int]]
-testField = [
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0,0],
-        [0,0,0,1,0,0,0,0,0,0],
-        [0,1,1,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
+--This makes a hardcoded test field.
+--Usefull for testing individual functions
+testField :: [[Cell]]
+testField = map (map numToCell) $ [
+        [0,1,0],
+        [0,1,0],
+        [0,1,0],
       ]
-
-
-randomField :: Int -> Int -> Int -> [[Int]]
-randomField mx my seed = [[ l !! ((y * x) + x) | x <- [0..(mx - 1)]] | y <- [0..(my - 1)]]
-  where l = randomRs (0, 1) (mkStdGen seed)
-  
-nullField :: Int -> Int -> [[Int]]
-nullField sx sy = [[ 0 | x <- [0..(sx - 1)]] | y <- [0..(sy - 1)]]
-
-
-properPos :: [[Int]] -> (Int, Int) -> (Int, Int)
-properPos f (x,y) = (nx x, ny y)
   where 
-    ny i 
-      | i < 0 = ny $ (snd $ trueFieldSize f) + i
-      | i >= (snd $ trueFieldSize f)  = ny $ i - (snd $ trueFieldSize f)
-      | otherwise = i
-    nx i 
-      | i < 0 = nx $ (fst $ trueFieldSize f) + i
-      | i >= (fst $ trueFieldSize f) = nx $ i - (fst $ trueFieldSize f)
-      | otherwise = i
+    numToCell x = if x == 1 then Alive else Dead
 
-buildAdjList :: [[Int]] -> (Int, Int) -> [(Int, Int)] 
-buildAdjList f (x, y) = [ properPos f (j, k) | k <- [y - 1, y, y + 1], j <- [x - 1, x, x + 1], (j, k) /= (x,y)]
+--Converts a cell to a bool
+isAlive :: Cell -> Bool
+isAlive Alive = True
+isAlive _ = False
 
-countLive :: (Int, Int) -> [[Int]] -> Int
-countLive (x, y) field = sum [ field !! k !! j  | (j,k) <- buildAdjList field (x, y)]
+--Gets a certain cell from a field
+getCell :: (Int, Int) -> [[Cell]] -> Cell
+getCell (x, y) f = f !! y !! x
 
-isCellAlive :: (Int, Int) -> [[Int]] -> Int
-isCellAlive (x, y) field =
-  if field !! y !! x == 1 then
-    if countLive (x,y) field == 2 || countLive (x,y) field == 3 then 1
-    else 0
-  else
-    if countLive (x,y) field == 3 then 1
-    else 0 
-
-computeField :: [[Int]] -> [[Int]]
-computeField field = [[isCellAlive (j, k) field | j <- [0..(fst $ fieldSize field)]] | k <- [0..(snd $ fieldSize field)]]
-
-
-printNullField :: [[Int]] -> IO ()
-printNullField field = do {
-    setSGR [SetColor Foreground Vivid Red];
-    printNullTable $ snd $ fieldSize field;
+--Prints the cell at the cursour
+printCell :: Cell -> IO ()
+printCell cell = do {
+    if cell == Dead then do {
+      setSGR [SetColor Foreground Vivid Red];
+      putStr ". ";
+    }
+    else do {
+      setSGR [SetColor Foreground Vivid Green];
+      putStr "O ";
+    } ;
     setSGR [Reset];
   }
+
+--Sleeps for 100ms
+frameDelay :: IO ()
+frameDelay = threadDelay 100000;
+
+--This clears the screen
+cls :: IO ()
+cls = do {
+    clearScreen;
+    setCursorPosition 0 0;
+  }
+
+--This flips the given cell from dead to alive.
+toggleCell :: (Int, Int) -> [[Cell]] -> [[Cell]]
+toggleCell (x, 0) (r:rw) = (doRow x r) : rw
   where 
-    printNullTable 0 = putStr ""
-    printNullTable i = do { printNullRow $ fst $ fieldSize field; putStr "\n"; printNullTable (i - 1);}
+    doRow :: Int -> [Cell] -> [Cell]
+    doRow _ [] = []
+    doRow 0 (i:is) = if i == Alive then Dead:is else Alive:is 
+    doRow x (i:is) = i : (doRow (x - 2) is)
+toggleCell (x, y) (r:rw) = r : (toggleCell (x, y - 1) rw)
 
-    printNullRow 0 = putStr ""
-    printNullRow i = do { putStr ". "; printNullRow (i - 1); }
+
+{-------------------------}
+{- Computation Functions -}
+{-------------------------}
+
+--Generates a random field.
+randomField :: (Int, Int) -> Int -> [[Cell]]
+randomField (mx, my) seed = [[ num (x, y) | x <- [0..(mx - 1)]] | y <- [0..(my - 1)]]
+  where 
+    num :: (Int, Int) -> Cell
+    num (x, y) = if (randomRs (0, 1::Int) $ mkStdGen seed) !! ((y * x) + x) == 1 
+      then Alive
+      else Dead
+  
+--Generates an empty field
+nullField :: (Int, Int) -> [[Cell]]
+nullField (sx, sy) = [[ Dead | x <- [0..(sx - 1)]] | y <- [0..(sy - 1)]]
+
+--This wraps a number around a bound
+wrappedPos :: [[Cell]] -> (Int, Int) -> (Int, Int)
+wrappedPos f (x, y) = (x `modulo` width, y`modulo` height)
+  where
+    width = fst $ fieldSize f
+    height = snd $ fieldSize f
+
+    modulo :: Int -> Int -> Int
+    modulo n m | r < 0     = r + m
+           | otherwise = r
+      where r = n `mod` m
 
 
-printField2 :: [[Int]] -> [[Int]] -> Int -> Int -> IO ()
-printField2 fOld fNew x y
+--Builds a list of adjecent positions.
+buildAdjList :: [[Cell]] -> (Int, Int) -> [(Int, Int)] 
+buildAdjList f (x, y) = [ wrappedPos f (x + j, y + k) | k <- [-1..1], j <- [-1..1], (j, k) /= (0, 0)]
+
+--Counts number of living cells next to given cell
+countLive :: (Int, Int) -> [[Cell]] -> Int
+countLive p field = length $ filter isAlive [ getCell pos field | pos <- buildAdjList field p]
+
+--Figures out weather the curret cell is Alive or dead
+computeCell :: (Int, Int) -> [[Cell]] -> Cell
+computeCell pos field =
+  if numLiving == 3 || (getCell pos field == Alive && numLiving == 2) 
+    then Alive else Dead
+  where numLiving = countLive pos field
+
+--Runs one step of the simultation on the entire field
+computeField :: [[Cell]] -> [[Cell]]
+computeField field = [[computeCell (j, k) field | j <- [0..(fst $ fieldSize field)]] | k <- [0..(snd $ fieldSize field)]]
+
+--This only prints the changed cells between the two provided fields
+printFieldStep :: [[Cell]] -> [[Cell]] -> (Int, Int) -> IO ()
+printFieldStep fOld fNew pos@(x,y)
   | x <= (fst $ fieldSize fNew) && y <= (snd $ fieldSize fNew) = do {
-      if fOld !! y !! x /= fNew !! y !! x then
-        if fNew !! y !! x == 1 then live else dead
-      else
-        return ();
-      printField2 fOld fNew (x + 1) y;
+      if getCell pos fOld /= getCell pos fNew then draw else return ();
+      printFieldStep fOld fNew (x + 1, y);
     }
-  | x > (fst $ fieldSize fNew) = printField2 fOld fNew 0 (y + 1)
+  | x > (fst $ fieldSize fNew) = printFieldStep fOld fNew (0, y + 1)
   | y > (snd $ fieldSize fNew) = putStr ""
 
   where
-    dead = do {
-      setCursorPosition (y) (x * 2); --The * 2 is to make up for the " " when a char is printed.
-      setSGR [SetColor Foreground Vivid Red];
-      putStr ". ";
-      setSGR [Reset];
+    draw = do {
+      setCursorPosition y (x * 2) ; --The * 2 is to make up for the " " when a char is printed.
+      printCell $ getCell (x,y) fNew;
     }
-    live = do {
-      setCursorPosition (y) (x * 2);
-      setSGR [SetColor Foreground Vivid Green];
-      putStr "O ";
-      setSGR [Reset];
-    }
+
     
-printField :: [[Int]] -> IO ()
+--this prints the entire field
+printField :: [[Cell]] -> IO ()
 printField [] = putStr ""
 printField (x:xs) = do {
-    (px x);
+    printRow x;
     putStr "\n";
     printField xs;
   }
-
   where 
-    px (l:ls)
-      | l == 1 = do {
-        setSGR [SetColor Foreground Vivid Green];
-        putStr "O ";
-        px ls;
-        setSGR [Reset];
-      }
-      | otherwise = do {
-        setSGR [SetColor Foreground Vivid Red];
-        putStr ". ";
-        px ls;
-        setSGR [Reset];
-      }
-    px [] = putStr ""
+    printRow :: [Cell] -> IO()
+    printRow [] = putStr ""
+    printRow (l:ls) = do {
+      printCell l;
+      printRow ls;
+    }
 
-runGame :: Int -> [[Int]] -> IO ()
+
+{------------------}
+{- Game Fucntions -}
+{------------------}
+
+--this runs the simulation for the provided number of steps
+--using the provided field.
+runGame :: Int -> [[Cell]] -> IO ()
 runGame 0 field = do {
         setCursorPosition 0 0;
         printField field;
-        putStr "\n";
       }
 runGame x field = 
   if theSame then do {
@@ -149,127 +187,117 @@ runGame x field =
     }
   else do {
         setCursorPosition 0 0;
-        printField2 field newField 0 0; 
+        printFieldStep field newField (0, 0); 
         putStr "\n";
-        threadDelay 250000;
+        frameDelay;
         runGame (x - 1) newField ;
       }
       where 
         newField = computeField field
         theSame = field == newField
 
-
-
+--this runs the simulations for the provied number of steps
+--using a randomly generated field.
 runRandomGame :: Int -> IO () 
 runRandomGame steps = do {
     x <- randomRIO (0, 1);
     cls;
-    printField $ randomField 20 20 x;
-    threadDelay 250000;
+    printField $ randomField (20, 20) x;
+    frameDelay;
     setCursorPosition 0 0;
-    runGame steps $ randomField 20 20 x;
+    runGame steps $ randomField (20, 20) x;
   }
 
-runWithUser :: IO () 
-runWithUser = do {
+--this starts the main input loop and runs the simulation
+--using user input.
+runWithUserInput :: IO () 
+runWithUserInput = do {
     cls;
     putStr "Enter the width of the field: ";
     w <- getLine;
     putStr "\nAnd the heigth: ";
     h <- getLine;
     cls;
-    printField $ field w h;
+    printField $ null w h;
     putStr "\nWASD: Move   Space: Toggle Cell    q: Run\n";
     setCursorPosition 0 0;
     showCursor;
-    runInputT defaultSettings $ loop 0 0 $ field w h;
+    runInputT defaultSettings $ loop (0, 0) $ null w h;
   }
   where 
-    field :: String -> String -> [[Int]]
-    field w h = nullField (read w :: Int) (read h :: Int)
+    --this simply creates a null field
+    null :: String -> String -> [[Cell]]
+    null w h = nullField (read w :: Int, read h :: Int)
 
-    loop :: Int -> Int -> [[Int]] -> InputT IO ()
-    loop x y f = do {
-
+    --this is the main input loop. I got the basic code from the haskeline page.
+    loop :: (Int, Int) -> [[Cell]] -> InputT IO ()
+    loop pos@(x, y) f = do {
         liftIO $ setCursorPosition 0 0;
         liftIO $ printField f;
         outputStrLn "\nWASD: Move, Space: Toggle Cell, r: Run x Iterations, e: Step, q: quit\n";
-        liftIO $ setCursorPosition x y;
+        --liftIO $ putStr $ show (x,y);
+        liftIO $ setCursorPosition y (x * 2);
 
         minput <- getInputChar "";
-        clearPress x y f;
+        --x,y must be swapped due to field logic
+        clearPress (x, y) f;
+
         case minput of
           Nothing -> return ()
-          Just 'q' -> liftIO $ cls;
 
+          --R runs the simulation form here.
+          --due to how computations are done, can't return here after finishing.
           Just 'r' -> do {
-            liftIO $ setCursorPosition ((snd $ fieldSize f) + 2) 0;
-            liftIO $ putStr "\nEnter the number of iterations to run: ";
-            i <- (liftIO $ getLine);
-            liftIO $ cls;
-            liftIO $ printField f;
-            liftIO $ threadDelay 250000;
-            liftIO $ runGame (read i :: Int) f;
-          }
-
-          Just 'w' -> moveCur 'w' x y f
-          Just 'a' -> moveCur 'a' x y f
-          Just 's' -> moveCur 's' x y f
-          Just 'd' -> moveCur 'd' x y f
-
-          Just ' ' -> loop x y $ toggleCell y x f
-
+              liftIO $ setCursorPosition ((snd $ fieldSize f) + 2) 0;
+              liftIO $ putStr "\nEnter the number of iterations to run: ";
+              i <- (liftIO $ getLine);
+              liftIO $ cls;
+              liftIO $ printField f;
+              liftIO $ frameDelay;
+              liftIO $ runGame (read i :: Int) f;
+            }
+          --E steps the simulation one step, can be held down to run.
           Just 'e' -> do { 
               liftIO $ setCursorPosition 0 0;
-              liftIO $ printField2 f (computeField f) 0 0; 
+              liftIO $ printFieldStep f (computeField f) (0, 0); 
               liftIO $ putStr "\n";
-              liftIO $ threadDelay 250000;
-              loop x y $ computeField f; 
+              liftIO $ frameDelay;
+              loop pos $ computeField f; 
             }
 
-          Just input -> loop x y f
-      }
-    clearPress :: Int -> Int -> [[Int]] -> InputT IO() 
-    clearPress x y f = 
-      if f !! (quot y 2)!! x == 1 then 
-        do {
-          liftIO $ setSGR [SetColor Foreground Vivid Green];
-          liftIO $ setCursorPosition x y;
-          outputStr "O";
-          liftIO $ setSGR [Reset];
-        }
-      else
-        do {
-          liftIO $ setSGR [SetColor Foreground Vivid Red];
-          liftIO $ setCursorPosition x y;
-          outputStr ".";
-          liftIO $ setSGR [Reset];
-        }
+          Just 'q' -> liftIO $ cls;       --Quits simulation
+          Just 'w' -> moveCur 'w' pos f   --Move cursor up
+          Just 'a' -> moveCur 'a' pos f   --Move cursor left
+          Just 's' -> moveCur 's' pos f   --Move cursor down
+          Just 'd' -> moveCur 'd' pos f   --Move cursor right
 
-    moveCur :: Char -> Int -> Int -> [[Int]] -> InputT IO()
-    moveCur c x y f = do {
-      if      c == 'a' && y > 0 then                         loop x (y - 2) f;
-      else if c == 'w' && x > 1 then                         loop (x - 1) y f;
-      else if c == 'd' && y < ((snd $ fieldSize f) * 2) then loop x (y + 2) f;
-      else if c == 's' && x < (fst $ fieldSize f) then       loop (x + 1) y f;
-      else                                                   loop x y f;
+          --Toggle cell between dead/alive
+          --X and y must be swapped because of how the field actually stores cells vs the display
+          Just ' ' -> loop pos $ toggleCell (x * 2, y) f 
+
+          Just input -> loop pos f --All other input does nothing
+      }
+    
+    --When a key is pressed in the main input loop, it's still written to screen.
+    --this function simply over-writes that keystroke with what's supposed to be
+    --in that field slot
+    clearPress :: (Int, Int) -> [[Cell]] -> InputT IO() 
+    clearPress (x, y) f = do {
+        liftIO $ setCursorPosition y (x * 2);
+        liftIO $ printCell $ getCell (x, y) f;
+      }
+
+    --this function actualy moves the physical cursor
+    moveCur :: Char -> (Int, Int) -> [[Cell]] -> InputT IO()
+    moveCur c (x, y) f = do {
+      if      c == 'w' && y > 0 then                         loop (x, y - 1) f;
+      else if c == 'a' && x > 1 then                         loop (x - 1, y) f;
+      else if c == 's' && y < (snd $ fieldSize f) then       loop (x, y + 1) f;
+      else if c == 'd' && x < (fst $ fieldSize f) then       loop (x + 1, y) f;
+      else                                                   loop (x, y) f;
     }
 
-cls :: IO()
-cls = do {
-    clearScreen;
-    setCursorPosition 0 0;
-  }
 
-
-toggleCell :: Int -> Int -> [[Int]] -> [[Int]]
-toggleCell x 0 (r:rw) = (doRow x r) : rw
-  where 
-    doRow _ [] = []
-    doRow 0 (i:is) = if i == 1 then 0:is else 1:is 
-    doRow x (i:is) = i : (doRow (x - 2) is)
-toggleCell x y (r:rw) = r : (toggleCell x (y - 1) rw)
-
-
-
---NEEED TESTS FOR GOOD GRADE!!!!!
+{-----------}
+{- Testing -}
+{-----------}
